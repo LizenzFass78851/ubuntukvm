2482d522d213cacebac406f84b36c6be36df1b6d
LizenzFass78851 <82592556+LizenzFass78851@users.noreply.github.com>
2022-10-03 08:14:23 +0200

    [skip ci]

--- startup.sh
+++ startup.sh
@@ -21,8 +21,8 @@ iptables -P OUTPUT ACCEPT
 iptables -A FORWARD -i eth0 -o virbr1 -p tcp --syn --dport 3389 -m conntrack --ctstate NEW -j ACCEPT
 iptables -A FORWARD -i eth0 -o virbr1 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 iptables -A FORWARD -i virbr1 -o eth0 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 3389 -j DNAT --to-destination $(nmap -v  192.168.121.1/24 -p 3389 | grep "Scanning 192.168.121." | cut -c 10-24)
-iptables -t nat -A POSTROUTING -o virbr1 -p tcp --dport 3389 -d $(nmap -v  192.168.121.1/24 -p 3389 | grep "Scanning 192.168.121." | cut -c 10-24) -j SNAT --to-source 192.168.121.1
+iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 3389 -j DNAT --to-destination $(nmap -v  192.168.121.1/24 -p 3389 | grep "Scanning 192.168.121." | cut -c 10-24 | head -1)
+iptables -t nat -A POSTROUTING -o virbr1 -p tcp --dport 3389 -d $(nmap -v  192.168.121.1/24 -p 3389 | grep "Scanning 192.168.121." | cut -c 10-24 | head -1) -j SNAT --to-source 192.168.121.1
 
 iptables -D FORWARD -o virbr1 -j REJECT --reject-with icmp-port-unreachable
 iptables -D FORWARD -i virbr1 -j REJECT --reject-with icmp-port-unreachable
